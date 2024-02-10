2e92f4b7f8a13818fb99566e2bfc573a9a9262e3
Matheus Rodrigues Silveira de Freitas <matheus_rf1997@hotmail.com>
2024-02-09 21:51:47 -0300

    pedeai-1: novas validacoes de senha, criacao de layout

--- app/Livewire/RegistroUsuario.php
+++ app/Livewire/RegistroUsuario.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 use Illuminate\Validation\Rules;
 use Illuminate\Auth\Events\Registered;
 use App\Providers\RouteServiceProvider;
+use App\Rules\ValidaSenha;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Livewire\Attributes\Layout;
@@ -14,23 +15,17 @@ use Livewire\Attributes\Validate;
 
 class RegistroUsuario extends Component
 {
-    #[Validate(['required', 'string', 'max:255'])]
+    #[Validate(['required', 'string', 'max:255', 'min:3'])]
     public string $name = '';
 
     #[Validate(['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class])]
     public string $email = '';
 
+    #[Validate(['required', 'confirmed', 'min:8', 'max:32', new ValidaSenha])]
     public string $password = '';
 
+    #[Validate(['required', 'string', 'min:8', 'max:32', new ValidaSenha])]
     public string $password_confirmation = '';
-
-    public function rules()
-    {
-        return [
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'password_confirmation' => ['required', 'string', Rules\Password::defaults()]
-        ];
-    }
 
     #[Layout('layouts.guest')]
     public function render()
@@ -46,9 +41,10 @@ class RegistroUsuario extends Component
             'name' => $this->name,
             'email' => $this->email,
             'password' => Hash::make($this->password),
+            'email_verified_at' => date('Y-m-d H:i:s')
         ]);
 
-        event(new Registered($user));
+        //event(new Registered($user));
 
         Auth::login($user);
 
